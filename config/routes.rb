c5f8a4fbe767ecf240a72b03e1e464c936e97795
Devin Ludwig <devinludwig@gmail.com>
2017-02-15 16:32:08 -0800

    fix routes to allow new user path; begin setup for factorygirl

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:new, :show] do
+  resources :users, :only => [:new, :show, :create] do
     resources :questions
     resources :answers, :only => []
   end
